be4c854a66e404c1f34916eb752cfc4c935c2da6
zahrablend <zahra@LAPTOP-010BK67M>
2023-07-02 13:10:36 +0300

    Add exception handling

--- HM81_JSON/StudentData.cs
+++ HM81_JSON/StudentData.cs
@@ -2,18 +2,24 @@
 
 namespace HM81_JSON
 {
+    // Exceptions best practices: https://learn.microsoft.com/en-us/dotnet/standard/exceptions/best-practices-for-exceptions
     public class StudentData
     {
         private readonly string _fileName;
 
         public StudentData(string fileName)
         {
+            // Validate method arguments (if the fileName parameter is null or empty)
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentException("File name cannot be null or empty.", nameof(fileName));
+            }
             _fileName = fileName;
         }
 
         public Student Load()
         {
-            // return JsonSerializer.Deserialize<Student>(json) gives warning CS8603: nullabe refference type
+            // Return JsonSerializer.Deserialize<Student>(json) gives warning CS8603: nullabe refference type
             try
             {
                 string json = File.ReadAllText(_fileName);
@@ -22,6 +28,7 @@ namespace HM81_JSON
                     throw new InvalidOperationException("File is empty");
                 }
                 var student = JsonSerializer.Deserialize<Student>(json);
+                // Check if student parameter is null when loading data
                 if (student == null)
                 {
                     throw new InvalidOperationException("Failed to deserialize data");
@@ -31,7 +38,7 @@ namespace HM81_JSON
             
             catch (JsonException exc)
             {
-                throw new InvalidOperationException("Invalid JSOn data", exc);
+                throw new InvalidOperationException("Invalid JSON data", exc);
             }
 
             catch (FileNotFoundException exc)
@@ -43,6 +50,11 @@ namespace HM81_JSON
 
         public void Save(Student student)
         {
+            // Check if student parameter is null when saving data
+            if (student == null)
+            {
+                throw new ArgumentNullException(nameof(student));
+            }
             string json = JsonSerializer.Serialize(student);
             File.WriteAllText(_fileName, json);
         }
